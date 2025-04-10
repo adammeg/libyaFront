86b761721d66d6737e302da494ea475827791873
adammeg <adam.bhedj13@gmail.com>
2025-04-10 14:16:15 +0100

    change language

---
+++ i18n.js
@@ -0,0 +1,11 @@
+// This file is required by next-translate-plugin
+module.exports = {
+    locales: ['en', 'ar'],
+    defaultLocale: 'ar',
+    pages: {
+      '*': ['common'],
+      '/': ['home'],
+      '/blog': ['blog'],
+      '/admin': ['admin'],
+    }
+  }
